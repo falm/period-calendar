0e9ac9ae92e3af9806c121fd0d43de5d0c597966
Jason Hou <hjj1992@gmail.com>
2016-09-04 22:23:51 +0800

    improve

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
 
 function getPeriods() {
     // var result = window.AndroidMessage.getMsg();
-    return [{start_on: '2016-08-08', duration: 5}, {start_on: '2016-08-29', duration: 5}];
+    return [{start_on: '2016-08-08', duration: 5}, {start_on: '2016-09-29', duration: 5}];
     // return eval(result);
+}
+
+function getTitle() {
+    var result = {title: '18 days Left'};
+    // var result = window.AndroidMessage.getMsg();
+    return result.title;
 }
 
 function processSameMonth(periods, calendar) {
@@ -12,10 +18,10 @@ function processSameMonth(periods, calendar) {
     if(m.isSame(e, 'month')){
 
         calendar = _.drop(calendar);
-        var heigh = function (data){
-            return assignHeighlight(data, getPeriodRange(new Date(periods[0].start_on), periods[0].duration));
+        var high = function (data){
+            return assignHeightLight(data, getPeriodRange(new Date(periods[0].start_on), periods[0].duration));
         };
-        calendar[0].data = _.flowRight(convertToTable, heigh, _.flatten)(calendar[0].data);
+        calendar[0].data = _.flowRight(convertToTable, high, _.flatten)(calendar[0].data);
     }
 
     return calendar;
@@ -24,7 +30,7 @@ function processSameMonth(periods, calendar) {
 function constructPeriods(periods) {
 
     var duration = moment(periods[1].start_on).diff(moment(periods[0].start_on), 'month');
-    return _(_.map(_.times(duration - 1), function (i) {
+    return _(_.map(_.range(duration - 1), function (i) {
         var start_on = moment(periods[0].start_on).add(i + 1, 'months');
         return {start_on: start_on};
     })).tap(function (results) {
@@ -52,44 +58,57 @@ function crossMonth(periods) {
 
 var build =  _.flowRight(crossMonth, constructPeriods);
 
+function processToday(calendar) {
+    return _.map(calendar, function (day) {
+        if(moment().isSame(day.date, 'day')){
+            day.content = day.day;
+        }
+        return day;
+    });
+}
+
+function buildCalendar(times, currentDate) {
+    return _.map(_.range(times), function (day) {
+        var date = moment([currentDate.get('year'), currentDate.get('month') , day + 1]);
+        return {content: null, day: day + 1, blank: false, heighlight: false, date: date};
+    });
+}
+
 function processTerminal(period) {
     var currentDate = moment(period.start_on);
     var startOfMonth =  moment(period.start_on).startOf('month');
     var endOfMonth =  moment(period.start_on).endOf('month');
 
-    var calender = _.map(_.times(endOfMonth.date()), function (day) {
-        var date = moment([currentDate.get('year'), currentDate.get('month') , day + 1]);
-        return {content: null, day: day + 1, blank: false, heighlight: false, date: date};
-    });
+    var calendar = buildCalendar(endOfMonth.date(), currentDate);
 
     if (period.hasOwnProperty('duration')){
-        calender = _.map(calender, function (day) {
-            if(moment().isSame(day.date, 'day')){
-                day.content = day.day;
-            }
-            return day;
-        });
+        calendar = processToday(calendar);
         var description = period.prefix + ' Period: ' + currentDate.format('ddd, MMM D');
     }
 
 
-    var blank = {blank: true};
-    calender = _.times(startOfMonth.weekday(), _.constant(blank)).concat(calender);
+    calendar = _.times(startOfMonth.weekday(), _.constant({blank: true})).concat(calendar);
 
-    calender = assignHeighlight(calender, getPeriodRange(new Date(period.start_on), period.duration));
-    calender = convertToTable(calender);
-    return {title: currentDate.format('MMM').toUpperCase(), description: description, month: currentDate.month(), data: calender};
+    calendar = assignHeightLight(calendar, getPeriodRange(new Date(period.start_on), period.duration));
+    calendar = convertToTable(calendar);
+    return {
+        title: currentDate.format('MMM YYYY').toUpperCase(),
+        description: description,
+        month: currentDate.month(),
+        param: currentDate.format('YYYY-MM'),
+        data: calendar
+    };
 
 }
 
 
 function getPeriodRange(date, duration) {
-    return _.map(_.times(duration), function (n) {
+    return _.map(_.range(duration), function (n) {
         return date.getDate() + n;
     });
 }
 
-function assignHeighlight(month, range) {
+function assignHeightLight(month, range) {
     var first = true;
    return _.map(month, function (day) {
 
@@ -124,6 +143,11 @@ function processTable(table, fun) {
      return convertToTable(_.map(_.flatten(table), fun));
 }
 
+function crossMonthHeightLight(periods, contexts) {
+    _.last(contexts).data = h(_.last(periods), _.last(contexts).data);
+    return contexts;
+}
+
 function renderCalender(context) {
 
     var source = $("#entry-template").html();
@@ -139,21 +163,30 @@ var buildContext = _.flowRight(_.partialRight(_.map, processTerminal), build);
 
 var renderCalendars = _.partialRight(_.each, renderCalender);
 
+function behavior() {
+
+    $('.calender').on('click', function () {
+    var link = 'moon://date/' + $(this).data('param');
+    console.log('open:' + link);
+        // window.open(link);
+    });
+
+    $('.current-date-false').parent().addClass('current-date-li');
+
+    $('.title').text(getTitle());
+}
+
 (function (periods) {
 
     var contexts = buildContext(periods);
 
-    contexts[contexts.length - 1].data = h(_.last(periods), _.last(contexts).data);
+    contexts = crossMonthHeightLight(periods, contexts);
 
     contexts = processSameMonth(periods, contexts);
 
     renderCalendars(contexts);
 
-    $('.calender').on('click', function () {
-        var link = 'moon://date/2016-09';
-        alert('open:' + link);
-        window.open(link);
-    })
+    behavior();
 
 })(
     getPeriods()
