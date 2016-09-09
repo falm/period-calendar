8dd063c3dfa32c4b884edc386d018d90605c7a6b
Jason Hou <hjj1992@gmail.com>
2016-09-09 21:25:56 +0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: './dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: 'style!css'},
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components|vendor|dist)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './src/index.html', to: 'index.html'}
+        ]),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
+};
