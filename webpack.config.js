033a1ee715bd26d3fdb4c4957c0ef1a63c8886ee
Jason Hou <hjj1992@gmail.com>
2016-10-22 15:50:05 +0800

    add env

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,43 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var webpack = require("webpack");
 var path = require('path');
 
+var isProd = (process.env.NODE_ENV === 'production');
+
+function getPlugins() {
+    var plugins = [];
+
+    plugins.push(new webpack.DefinePlugin({
+        'process.env': {
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        }
+    }));
+
+    plugins.push(new CopyWebpackPlugin([
+        { from: './src/index.html', to: 'index.html'},
+        { from: './src/css/style.css', to: 'style.css'}
+    ]));
+
+    plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+
+    if (isProd) {
+        plugins.push(new webpack.optimize.UglifyJsPlugin());
+    } else {
+    }
+
+    return plugins;
+}
+
 module.exports = {
-    entry: './src/js/main.js',
+    entry: {
+        bundle: './src/js/main.js',
+    },
     output: {
         path: './dist/',
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: 'style!css'},
+            // {test: /\.css$/, loader: 'style!css'},
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components|vendor|dist)/,
@@ -19,15 +47,9 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
-            }
+            },
         ]
     },
 
-    plugins: [
-        new CopyWebpackPlugin([
-            { from: './src/index.html', to: 'index.html'}
-        ]),
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        // new webpack.optimize.UglifyJsPlugin({minimize: true})
-    ]
+    plugins: getPlugins()
 };
